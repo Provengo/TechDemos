67f2ef49b1c5a4de2eb30f16d9719b029180a85e
geraw <gera.weiss@gmail.com>
2023-12-08 10:34:09 +0200

    Add script to start Selenium Server

--- runMagento.sh
+++ runMagento.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+if ! pgrep -f selenium-server-4.1.2.jar > /dev/null
+then
+    echo "Starting Selenium Server"
+    nohup java -jar selenium-server-4.1.2.jar standalone > /dev/null 2>&1 &
+fi
+
 ./provengo.sh run --show Magento | awk '/switching to dry run/{exit} 1'
