2023b23b7bd6319ad0a77059c3ba5f9740cb2e2d
geraw <gera.weiss@gmail.com>
2023-12-08 10:35:05 +0200

    Update product quantities and add new products

--- Magento/spec/js/tests.story.js
+++ Magento/spec/js/tests.story.js
@@ -10,8 +10,8 @@
 //const URL = "http://localhost:10000"
 const URL = "https://master-7rqtwti-c5v7sxvquxwl4.eu-4.magentosite.cloud/"
 
-const NUM_OF_USERS = 1
-const NUM_OF_SESSIONS = 1
+const NUM_OF_USERS = 2
+const NUM_OF_SESSIONS = 2
 const NUM_OF_PROD = 3
 
 // Add a b-thread for each user in the users array
@@ -22,8 +22,8 @@ for (let i = 0; i < NUM_OF_SESSIONS; i++) {
             with (new SeleniumSession().start(URL)) {
                 login(user)
                 for (let k = 0; k < NUM_OF_PROD; k++) {
-                    //let product = getRandomProduct()
-                    product = products[0]
+                    let product = choose(products)
+                    //product = products[product]
                     addToCart({ product: product, user: user })
                 }
             }
