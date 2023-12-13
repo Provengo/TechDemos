fcd4b3b610bf4e6384a9acdfdc5fa33b29bda11a
geraw <gera.weiss@gmail.com>
2023-12-13 08:57:48 +0200

    Update provengo.sh command in runMagento.sh to run the before script.

--- Magento/empty_cart.py
+++ Magento/empty_cart.py
@@ -2,17 +2,22 @@ print("enter")
 import json
 from requests import post, get, delete, put
 
-# URL = "https://magento2-b2b.magebit.com/"
-URL = "https://magento2-demo.magebit.com/"
 # URL = "http://localhost/"
+URL= "https://master-7rqtwti-c5v7sxvquxwl4.eu-4.magentosite.cloud/"
 CREDENTIALS = [
     {'username': "roni_cost@example.com", 'password': "roni_cost3@example.com"},
     {'username': "david_lowcost@example.com", 'password': "david_lowcost3@example.com"},
 ]
-print("10.. Statrting")
+print("Connecting to magento...")
 # try:
 for credentials in CREDENTIALS:
+    print(f'Emptying cart for {credentials["username"]}...')
     r = post(f'{URL}/rest/default/V1/integration/customer/token', params=credentials)
+
+    if r.status_code != 200:
+        print(f'Error: {r.text}')
+        continue
+
     token = r.text[1:-1]
     header = {'Authorization': f'Bearer {token}'}
 
@@ -23,27 +28,27 @@ for credentials in CREDENTIALS:
         for item in cart["items"]:
             delete(f'{URL}/rest/default/V1/carts/mine/items/{item["item_id"]}', headers=header)
 
-# Add products
-CREDENTIALS = {'username': "user", 'password': "bitnami1"}
-# CREDENTIALS = {'username': "user", 'password': "user123"}
+# # Add products
+# CREDENTIALS = {'username': "user", 'password': "bitnami1"}
+# # CREDENTIALS = {'username': "user", 'password': "user123"}
 
-r = post(f'{URL}/rest/V1/integration/admin/token', params=CREDENTIALS)
-token = r.text[1:-1]
-header = {'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'}
+# r = post(f'{URL}/rest/V1/integration/admin/token', params=CREDENTIALS)
+# token = r.text[1:-1]
+# header = {'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'}
 
-for prod in ["MH07-XL-Green", "MH07-L-Green", "MS11-S-Green", "MSH03-36-Green", "WS04-M-Green", "WT06-XS-Yellow",
-             "WJ07-XS-Orange", "WJ07-M-Orange"]:
-    data = {"product": {"sku": prod, "extension_attributes": {
-        "stock_item": {"qty": 100, "stock_status_changed_auto": 0, "backorders": "1", "use_config_backorders": False}}}}
+# for prod in ["MH07-XL-Green", "MH07-L-Green", "MS11-S-Green", "MSH03-36-Green", "WS04-M-Green", "WT06-XS-Yellow",
+#              "WJ07-XS-Orange", "WJ07-M-Orange"]:
+#     data = {"product": {"sku": prod, "extension_attributes": {
+#         "stock_item": {"qty": 100, "stock_status_changed_auto": 0, "backorders": "1", "use_config_backorders": False}}}}
 
-    r = put(f'{URL}/rest/default/V1/products/{prod}', headers=header, data=json.dumps(data))
+#     r = put(f'{URL}/rest/default/V1/products/{prod}', headers=header, data=json.dumps(data))
 
-# Reset product to original price (39$)
-prod = "WT06-XS-Yellow"
-data = {"product": {"price": "39"}}
-r = put(f'{URL}/rest/default/V1/products/{prod}', headers=header, data=json.dumps(data))
+# # Reset product to original price (39$)
+# prod = "WT06-XS-Yellow"
+# data = {"product": {"price": "39"}}
+# r = put(f'{URL}/rest/default/V1/products/{prod}', headers=header, data=json.dumps(data))
 
-#remove new product named "test"
-prod = 'test-men-top-tee-XS-Yellow'
-delete(f'{URL}/rest/default/V1/products/{prod}', headers=header)
+# #remove new product named "test"
+# prod = 'test-men-top-tee-XS-Yellow'
+# delete(f'{URL}/rest/default/V1/products/{prod}', headers=header)
 
--- runMagento.sh
+++ runMagento.sh
@@ -5,4 +5,4 @@ then
     nohup java -jar selenium-server-4.1.2.jar standalone > /dev/null 2>&1 &
 fi
 
-./provengo.sh run --show --dev-mode Magento
+./provengo.sh run --dev-mode --show --before="python3 Magento/empty_cart.py" Magento
