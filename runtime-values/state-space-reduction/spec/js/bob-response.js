7aad2ccc25b4457b7594822cc6260daa02e0feea
Michael Bar-Sinai <mich.barsinai@gmail.com>
2025-03-25 11:37:52 -0400

    Improved model alignment with textual requirement

--- runtime-values/state-space-reduction/spec/js/bob-response.js
+++ runtime-values/state-space-reduction/spec/js/bob-response.js
@@ -2,10 +2,16 @@
 
 /**
  * When Bob is selected, greet him with a "Hi!" after the regular process is done.
- * 
  */
+
+const bobSelected = EventSet("Bob selected", function(e){
+    return e?.data?.lib == "bp-base" &&
+            e?.data?.type == "choice" &&
+            e.data.value.name == "Bob";
+})
+
 bthread("respond to bob", function(){
-    waitFor( choiceEvent(PEOPLE[1]) );
+    waitFor( bobSelected );
     waitFor( MAIN_FLOW_DONE );
     request(Event("Hi, Bob!"));
 });
