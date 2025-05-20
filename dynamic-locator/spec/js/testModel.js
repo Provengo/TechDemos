e2a89b7181e4fd5ed76d9341f58b5dd6e9a4e4ec
Michael Bar-Sinai <mich.barsinai@gmail.com>
2025-05-20 01:09:19 -0400

    Comments

--- dynamic-locator/spec/js/testModel.js
+++ dynamic-locator/spec/js/testModel.js
@@ -27,7 +27,11 @@ function checkNewestItem(itemName){
     
     // Sanitizing the RTV variable name. RTV vars need to be valid JS variable names.
     const rtvName = "s"+itemName.replaceAll(/[^a-zA-Z1-9]/g,"_");
+    
+    // The runtime variable name is rtvName, so wwe need both @{} and ${}
     const liLocator = `//li[@id='@{${rtvName}}']`;
+    
+    // Check the row functionality
     bthread(`check-item-${rtvName}`, function(){
         // first, get the dynamic row id. Block new rows from being generated while at it.
         block(Ctrl.markEvent("Adding Item"), function(){
@@ -36,6 +40,7 @@ function checkNewestItem(itemName){
             }, captureItem);
         });
 
+        // We're safe - resume concurrency, and check functionality as needed.
         SESSION.waitForVisibility(liLocator);
         if ( maybe(`complete-${rtvName}`) ) {
             SESSION.click(`${liLocator}/button`);
@@ -44,10 +49,21 @@ function checkNewestItem(itemName){
     });
 }
 
+/**
+ * Takes the last list item, stores its id in the 
+ * itemName runtime variable;
+ */
 function captureItem(){
+    // Gets all `li`s. (real world might need to limit the scope of the search).
     let items = document.querySelectorAll("li");
+
+    // Get the id of the last item
     let newItem = items.item(items.length-1);
     let newItemId = newItem.id;
+
+    // Now store in the runtime variable
     pvg.rtv.set(pvg.params.itemName,newItemId);
+
+    // logging is fun
     pvg.log("Id of new item: " + newItemId);
 }
